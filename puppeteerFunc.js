316c50fbd1eabf6224121b0972c35901c78141de
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-03-31 17:47:55 +1300

    Login to Simplicity

--- puppeteerFunc.js
+++ puppeteerFunc.js
@@ -1,9 +1,17 @@
 const puppeteer = require('puppeteer');
 
 const puppeteerFunc = async () => {
-  const browser = await puppeteer.launch({ executablePath: "/opt/homebrew/bin/chromium" });
+  const browser = await puppeteer.launch({ executablePath: process.env.PUPPETEER_EXECUTABLE_PATH });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+
+  await page.goto('https://app.simplicity.kiwi/login');
+
+  await page.type('#email', process.env.SIMPLICITY_EMAIL);
+  await page.type('#password', process.env.SIMPLICITY_PASSWORD);
+  await page.keyboard.press('Enter');
+
+  await page.waitForNavigation();
+
   await page.screenshot({path: 'example.png'});
 
   await browser.close();
