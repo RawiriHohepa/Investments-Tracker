9b5db40edda240bbd65e3763ea1ad5825bb4e9b6
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-03-31 17:51:04 +1300

    Move Simplicity URL to .env

--- puppeteerFunc.js
+++ puppeteerFunc.js
@@ -4,13 +4,14 @@ const puppeteerFunc = async () => {
   const browser = await puppeteer.launch({ executablePath: process.env.PUPPETEER_EXECUTABLE_PATH });
   const page = await browser.newPage();
 
-  await page.goto('https://app.simplicity.kiwi/login');
+  await page.goto(process.env.SIMPLICITY_URL);
 
   await page.type('#email', process.env.SIMPLICITY_EMAIL);
   await page.type('#password', process.env.SIMPLICITY_PASSWORD);
   await page.keyboard.press('Enter');
 
   await page.waitForNavigation();
+  await page.waitForSelector('button');
 
   await page.screenshot({path: 'example.png'});
 
